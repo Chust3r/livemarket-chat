8ef3311c768a5c57ebc6f11ed6a3923e575e7b09
Chust3r <kriptoortega@gmail.com>
2024-11-21 15:17:49 -0600

    Implemnet login endpoint

--- app/routes/auth/login.ts
+++ app/routes/auth/login.ts
@@ -1,5 +1,87 @@
 import { Hono } from 'hono'
+import { getUserByEmail } from '~actions/user'
+import { createJwt } from '~lib/jwt'
+import { verifyPassword } from '~lib/password'
+import { makeResponse } from '~lib/response'
+import { validate, loginSchema } from '~lib/validation'
 
 export const login = new Hono().basePath('/login')
 
-login.post('/', async (c) => {})
+//→ ENPOINT TO LOGIN
+
+login.post('/', async (c) => {
+	try {
+		//→ PARSE BODY TO JSON
+
+		const body = await c.req.json()
+
+		//→ VALIDATE BODY
+
+		const { success, issues, output } = validate(loginSchema, body)
+
+		if (!success) {
+			const errors = issues.reduce((acc, issue) => {
+				// @ts-ignore
+				acc[issue.path[0]] = issue.message
+				return acc
+			}, {})
+
+			const response = makeResponse('error', 'Validation error', {
+				errors,
+			})
+
+			//→ RETURN RESPONSE
+
+			return c.json(response, 400)
+		}
+
+		//→ CHECK IF USER EXISTS
+
+		const user = await getUserByEmail(output.email)
+
+		if (!user) {
+			const response = makeResponse('error', 'Invalid credentials', {})
+
+			return c.json(response, 401)
+		}
+
+		//→ CHECK IF PASSWORD IS CORRECT
+
+		const isPasswordCorrect = await verifyPassword(
+			output.password,
+			user.password
+		)
+
+		//→ IF PASSWORD IS NOT CORRECT RETUNR UNAUTHORIZED
+
+		if (!isPasswordCorrect) {
+			const response = makeResponse('error', 'Invalid credentials', {})
+
+			return c.json(response, 401)
+		}
+
+		//→ GENERATE JWT TOKEN
+
+		const token = await createJwt({
+			userId: user.id,
+			username: user.username,
+			email: user.email,
+		})
+
+		//→ RETURN RESPONSE
+
+		const response = makeResponse('success', 'Login successful', {
+			data: {
+				token,
+			},
+		})
+
+		return c.json(response, 200)
+	} catch (e) {
+		console.log('[USER LOGIN] ERROR', e)
+
+		const response = makeResponse('error', 'Something went wrong', {})
+
+		return c.json(response, 500)
+	}
+})
