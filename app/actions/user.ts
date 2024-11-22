3ee6d4b9d726156147f03a4e969c54599659fd8a
Chust3r <kriptoortega@gmail.com>
2024-11-22 10:31:50 -0600

    Implement user info endpoint

--- app/actions/user.ts
+++ app/actions/user.ts
@@ -25,3 +25,19 @@ export const createUser = async (user: User) => {
 
 	return createdUser
 }
+
+export const getUserInfo = async (id: string) => {
+	const user = await db.query.users.findFirst({
+		where: (users, { eq }) => eq(users.id, id),
+		columns: {
+			id: true,
+			email: true,
+			username: true,
+			avatarUrl: true,
+			status: true,
+			createdAt: true,
+		},
+	})
+
+	return user
+}
--- app/routes/api/users.ts
+++ app/routes/api/users.ts
@@ -1,7 +1,35 @@
 import { Hono } from 'hono'
+import { getUserInfo } from '~actions/user'
+import { makeResponse } from '~lib/response'
 
 export const usersRouter = new Hono().basePath('/users')
 
 //→ GET INFO FROM USER (ONLY PUBLIC INFO)
 
-usersRouter.get('/:id', async (c) => {})
+usersRouter.get('/:id', async (c) => {
+	try {
+		//→ GET PARAM (USER ID)
+
+		const id = c.req.param('id')
+
+		//→ GET USER INFO
+
+		const user = await getUserInfo(id)
+
+		//→ CREATE RESPONSE
+
+		const response = makeResponse('success', 'User info', {
+			data: user,
+		})
+
+		//→ RETURN RESPONSE
+
+		return c.json(response, 200)
+	} catch (e) {
+		console.log('[USER INFO] ERROR', e)
+
+		const response = makeResponse('error', 'Something went wrong', {})
+
+		return c.json(response, 500)
+	}
+})
