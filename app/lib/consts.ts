a3c30397d1e2d5898f9f26206b73b8f5a38f98a9
Chust3r <kriptoortega@gmail.com>
2024-11-23 12:50:08 -0600

    Implement PocketBase client

--- app/lib/consts.ts
+++ app/lib/consts.ts
@@ -7,3 +7,6 @@ export const DB_FILE_NAME = process.env.DB_FILE_NAME || 'file:local.db'
 export const JWT_SECRET = process.env.JWT_SECRET || 'jwt_secret'
 
 export const JWT_EXPIRATION_TIME = process.env.JWT_EXPIRATION_TIME || '24h'
+
+export const POCKETBASE_URL =
+	process.env.POCKETBASE_URL || 'http://localhost:8090'
