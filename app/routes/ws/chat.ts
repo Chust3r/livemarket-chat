df9f765b7256b58873ee0407b65c13b740c6596c
Chust3r <kriptoortega@gmail.com>
2024-11-25 19:27:30 -0600

    Update Drizzle schema with new syntax

--- app/routes/ws/chat.ts
+++ app/routes/ws/chat.ts
@@ -1,11 +1,28 @@
-import type { Server,Socket } from 'socket.io'
+import type { Server, Socket } from 'socket.io'
+import { createOrGetPrivateChat } from '~actions/chat'
+import { validateUsersIds } from '~actions/user'
 
 export const chatWs = (io: Server) => {
 	//→ ON CONNECT
 
-	io.on('connect', (socket:Socket) => {
-		console.log('A user connected')
+	io.use((socket, next) => {
+		//→ ADD VALIDATION & GET CLIENT ID / VENDOR ID
 
-		
+		//→ JWT CONTAINS USER INFO, IF NOT RETURN ERROR
+
+		next()
+	})
+
+	//→ CONNECT A NEW USER TO THE CHAT
+
+	io.on('connect', (socket: Socket) => {
+		socket.on('chat', async (data) => {
+			const userId1 = '1f5569fb-a606-4137-b989-26b34b6c662e'
+			const userId2 = '538a28fe-7750-4814-937d-521519f2232c'
+
+			const chat = await createOrGetPrivateChat(userId1, userId2)
+
+			console.log(chat)
+		})
 	})
 }
--- drizzle/schema.ts
+++ drizzle/schema.ts
@@ -1,100 +1,117 @@
-import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core'
+import { sqliteTable } from 'drizzle-orm/sqlite-core'
 import { sql } from 'drizzle-orm'
 import { randomUUID } from 'node:crypto'
 
 // → CHATS
 
-export const chats = sqliteTable('chats', {
-	id: text()
+export const chats = sqliteTable('chats', (t) => ({
+	id: t
+		.text()
 		.primaryKey()
 		.$default(() => randomUUID()),
-	type: text({
-		enum: ['private', 'group'],
-	}).notNull(),
-	name: text().notNull(),
-	createdAt: text().default(sql`(CURRENT_TIMESTAMP)`),
-	updatedAt: text()
+	type: t
+		.text({
+			enum: ['private', 'group'],
+		})
+		.notNull(),
+	name: t.text().notNull(),
+	createdAt: t.text().default(sql`(CURRENT_TIMESTAMP)`),
+	updatedAt: t
+		.text()
 		.default(sql`(CURRENT_TIMESTAMP)`)
 		.$onUpdate(() => sql`(CURRENT_TIMESTAMP)`),
-})
+}))
 
 // → USERS
 
-export const users = sqliteTable('users', {
-	id: text()
+export const users = sqliteTable('users', (t) => ({
+	id: t
+		.text()
 		.primaryKey()
 		.$default(() => randomUUID()),
-	username: text({ length: 30 }).notNull(),
-	email: text().notNull(),
-	password: text().notNull(),
-	avatarUrl: text(),
-	status: text({
-		enum: ['online', 'offline'],
-	})
+	username: t.text({ length: 30 }).notNull(),
+	email: t.text().notNull(),
+	password: t.text().notNull(),
+	avatarUrl: t.text(),
+	status: t
+		.text({
+			enum: ['online', 'offline'],
+		})
 		.default('online')
 		.notNull(),
-	createdAt: text().default(sql`(CURRENT_TIMESTAMP)`),
-	updatedAt: text()
+	createdAt: t.text().default(sql`(CURRENT_TIMESTAMP)`),
+	updatedAt: t
+		.text()
 		.default(sql`(CURRENT_TIMESTAMP)`)
 		.$onUpdate(() => sql`(CURRENT_TIMESTAMP)`),
-})
+}))
 
 // → MESSAGES
 
-export const messages = sqliteTable('messages', {
-	id: text()
+export const messages = sqliteTable('messages', (t) => ({
+	id: t
+		.text()
 		.primaryKey()
 		.$default(() => randomUUID()),
-	chatId: text()
+	chatId: t
+		.text()
 		.notNull()
-		.references(() => chats.id),
-	senderId: integer({ mode: 'number' })
+		.references(() => chats.id), //→ RELATION WITH CHATS
+	senderId: t
+		.integer({ mode: 'number' })
 		.notNull()
-		.references(() => users.id),
-	content: text().notNull(),
-	type: text({
-		enum: ['text', 'image', 'file'],
-	})
+		.references(() => users.id), //→ RELATION WITH USERS
+	content: t.text().notNull(),
+	type: t
+		.text({
+			enum: ['text', 'image', 'file'],
+		})
 		.notNull()
 		.default('text'),
-	isDeleted: integer({ mode: 'boolean' }).notNull().default(false),
-	isEdited: integer({ mode: 'boolean' }).notNull().default(false),
-	createdAt: text().default(sql`(CURRENT_TIMESTAMP)`),
-	updatedAt: text()
+	isDeleted: t.integer({ mode: 'boolean' }).notNull().default(false),
+	isEdited: t.integer({ mode: 'boolean' }).notNull().default(false),
+	createdAt: t.text().default(sql`(CURRENT_TIMESTAMP)`),
+	updatedAt: t
+		.text()
 		.default(sql`(CURRENT_TIMESTAMP)`)
 		.$onUpdate(() => sql`(CURRENT_TIMESTAMP)`),
-})
+}))
 
 // → PRODUCTS
 
-export const products = sqliteTable('products', {
-	id: text()
+export const products = sqliteTable('products', (t) => ({
+	id: t
+		.text()
 		.primaryKey()
 		.$default(() => randomUUID()),
-	name: text().notNull(),
-	description: text().notNull(),
-	price: integer({ mode: 'number' }).notNull(),
-	url: text().notNull(),
-	images: text({ mode: 'json' })
+	name: t.text().notNull(),
+	description: t.text().notNull(),
+	price: t.integer({ mode: 'number' }).notNull(),
+	url: t.text().notNull(),
+	images: t
+		.text({ mode: 'json' })
 		.notNull()
 		.$type<string[]>()
 		.default(sql`(json_array())`),
-	createdAt: text().default(sql`(CURRENT_TIMESTAMP)`),
-	updatedAt: text()
+	createdAt: t.text().default(sql`(CURRENT_TIMESTAMP)`),
+	updatedAt: t
+		.text()
 		.default(sql`(CURRENT_TIMESTAMP)`)
 		.$onUpdate(() => sql`(CURRENT_TIMESTAMP)`),
-})
+}))
 
 // → CHAT PARTICIPANTS
 
-export const chatParticipants = sqliteTable('chat_participants', {
-	chatId: text()
+export const chatParticipants = sqliteTable('chat_participants', (t) => ({
+	chatId: t
+		.text()
 		.notNull()
-		.references(() => chats.id),
-	userId: text()
+		.references(() => chats.id, { onDelete: 'cascade' }), //→ RELATION WITH CHATS
+	userId: t
+		.text()
 		.notNull()
-		.references(() => users.id),
-})
+		.references(() => users.id), //→ RELATION WITH USERS
+}))
 
 // → RELATIONS
 
